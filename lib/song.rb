b34cf6246c6b243a095f74e58c74befcc267f0e8
Mario Adamo Jr <marioadamojr@gmail.com>
2019-04-12 22:31:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,9 @@ class Song
   def self.new_by_filename(file_name)
     song, artist = file.split(" - ")
     new_song = self.new(song)
-    new_song.artist = artist
+    new_artist = Artist.find_or_create_by_name(name)
+    new_song.artist = new_artist
+    new_song
   end
 
 end
